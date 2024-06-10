1645401402807fdb29e8ae1400ff12f77e820e62
Jafnak <jafnakusman@gmail.com>
2024-06-10 11:54:01 +0530

    navbar  components added

--- src/components/Add.jsx
+++ src/components/Add.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 
+import Navbar from './Navbar'
+
+
 const Add = () => {
   return (
     <div>
-
+<Navbar/>
 <div className="container">
     <div className="row g-3">
         <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
