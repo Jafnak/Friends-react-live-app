7c60339705f48b4589efdb23191d9e945b5691f3
Jafnak <jafnakusman@gmail.com>
2024-06-10 11:15:08 +0530

    add components added

---
+++ src/components/Add.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+
+const Add = () => {
+  return (
+    <div>
+
+<div className="container">
+    <div className="row g-3">
+        <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+            <label htmlFor="" className="form-label">Name</label>
+            <input type="text" className="form-control" />
+        </div>
+        <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+        <label htmlFor="" className="form-label">Friend Name</label>
+        <input type="text" className="form-control" />
+        </div>
+        <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+        <label htmlFor="" className="form-label">Friend Nick Name</label>
+        <input type="text" className="form-control" />
+        </div>
+        <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+        <label htmlFor="" className="form-label">Describe friend</label>
+        <textarea name="" id="" className="form-control"></textarea>
+        </div>
+        <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+            <button className="btn btn-primary">Add</button>
+        </div>
+    </div>
+</div>
+
+    </div>
+  )
+}
+
+export default Add
